e4043da8e9df16f52a31a2c391d7fa79dd575862
Tom Nullet <Tom@Toms-MacBook-Pro.local>
2014-06-04 17:45:00 -0500

    model testing done

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Appointment do
-  it { should belong_to(:session) }
+  it { should belong_to(:session) } # ---- association works as desired with session belongs to appointment.
   it { should belong_to(:user).with_foreign_key('student_id')}
   it { should have_many(:comments)}
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+# Includes coveralls, yay for test coverage
+require 'coveralls'
+Coveralls.wear!
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -5,10 +9,6 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'shoulda/matchers'
 
-
-# Includes coveralls, yay for test coverage
-require 'coveralls'
-Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
