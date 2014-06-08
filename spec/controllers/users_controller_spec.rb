e7fa28b6896485fa23d7f9cd071bd1c81d3448d5
Casey Ann Cumbow <cumbow8@gmail.com>
2014-06-08 15:50:50 -0500

    tests user controller - all passing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,14 +3,26 @@ require 'spec_helper'
 describe UsersController do 
 
 	describe 'GET #new' do
-		it 'renders the new form'
+		it 'shows the new user form' do
+			get :new
+			expect(response.status).to eq 200 
+		end
 	end
 
 
 	describe '#create' do
-		it 'saves a new user to the database'
-		it 'redirects to login page if user is saved'
-		it 'redirects to signup if registering a new user fails'
+		it 'saves a new user to the database' do
+			post :create, user: {:first_name => "Penelope", :last_name => "Mitchell", :email => "p@p.com", :role => 'student', :cohort => 'nighthawk', :password => "pass"}
+			expect(response.status).to eq 302
+		end
+		it 'redirects to login page if user is saved' do
+			post :create, user: {:first_name => "Penelope", :last_name => "Mitchell", :email => "p@p.com", :role => 'student', :cohort => 'nighthawk', :password => "pass"}
+			expect(response).to redirect_to :root
+		end
+		it 'redirects to signup if registering a new user fails' do
+			post :create, user: {:last_name => "Mitchell", :role => 'student', :cohort => 'nighthawk', :password => "pass"}
+			expect(response).to redirect_to :signup
+		end	
 	end
 
 
